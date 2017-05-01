bfa94e5494864d9d684343487a6b98b81d6805e9
Spencer <spencer@poonwoo.com>
2017-05-01 14:03:25 -0400

    Tiles colour by their value

---
+++ Assets/Scripts/Colours.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colours : Singleton<Colours> {
+	protected Colours () {}
+
+	[SerializeField] private Color[] ranges;
+
+	public Color[] Ranges {
+		get {
+			return ranges;
+		}
+	}
+}
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Tile : MonoBehaviour {
 	public TextMeshProUGUI valueLabel;
 	public int value;
+	public Image bg;
 
 	private void Start() {
 		if(Random.value > 0.5f)
@@ -18,10 +20,20 @@ public class Tile : MonoBehaviour {
 			valueLabel.text = value.ToString();
 		else
 			Destroy(gameObject);
+
+		SetColour();
 	}
 
 	private void OnCollisionEnter2D(Collision2D col) {
 		value--;
 		RefreshLabel();
+	}
+
+	private void SetColour() {
+		float t = (float)value%10f / 10f;
+		int i = value/10 % 7;
+		int j = (i+1)%7;
+		Color c = Color.Lerp(Colours.Instance.Ranges[i],Colours.Instance.Ranges[j],t);
+		bg.color = c;
 	}
 }
